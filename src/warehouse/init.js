64d27583666e5af4b280e2295b2d25cd7ea0a252
airman5573 <byeongin@shoplic.kr>
2026-01-18 17:56:59 +0900

    backup

--- src/warehouse/init.js
+++
@@ -1,3 +0,0 @@
-module.exports = (app, WHQuery) => {
-  require('./warehouseRoute')(app, WHQuery);
-}
--- src/warehouse/warehouse-client/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const UPDATE_POST_INFO = 'update_post_info';
-export const ADD_POST_INFO = 'add_post_info';
-export const REMOVE_POST_INFO = 'remove_post_info';
